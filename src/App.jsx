cbcc0659e3fdf13df35653ddd541231f01af6a52
Armando Del Rio <39106560+adelriorz@users.noreply.github.com>
2022-09-25 04:04:47 +0000

    Adding initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import './App.css'
 import Header from './Header';
-import Content from './Content';
 import Footer from './Footer';
+import Employees from './Employees';
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Content />
+      <Employees />
       <Footer />
     </main>
   )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 const Header = () => {
-  return(
+  return (
     <header>
       <h1>Header</h1>
     </header>
