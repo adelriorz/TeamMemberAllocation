8a5192c8601b1d8c45b191cc8d7987ecf2504e95
Armando Del Rio <39106560+adelriorz@users.noreply.github.com>
2022-09-25 03:34:04 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css'
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <Content />
+      <Footer />
+    </main>
+  )
+}
